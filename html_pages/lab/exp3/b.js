36f72b9537fe21953c58488192748cf292e2e35c
Vaishali Singh <70578189+2020201070@users.noreply.github.com>
2020-11-20 20:11:26 +0530

    Add files via upload

---
+++ html_pages/lab/exp3/b.js
@@ -0,0 +1,166 @@
+	(function() {
+	  const myQuestions = [
+	    {
+	      question: " What is the command To Print The column 1 and 4 of a file ?",
+	      answers: {
+	        a: "awk '{print $0,$3}' employee.txt ",
+	        b: "column '{print $1,$4}' employee.txt ",
+	        c: "awk '{print $1,$4}' employee.txt "
+	      },
+	      correctAnswer: "c"
+	    },
+	    {
+	      question: "________ prints all the lines that have manager in it ?",
+	      answers: {
+	        a: "awk '/manager/ {print}' employee.txt ",
+	        b: "awk 'manager {print}' employee.txt ",
+	        c: "awk '/manager% {print}' employee.txt "
+	      },
+	      correctAnswer: "b"
+	    },
+	    {
+	      question: "Which of the following is command to count the lines in a file?",
+	      answers: {
+	        a: "awk 'END { count NR }' geeksforgeeks.txt",
+			b: "awk 'START { print NR }' geeksforgeeks.txt",
+	        c: "awk 'END { print NR }' geeksforgeeks.txt",
+	        
+	      },
+	      correctAnswer: "c"
+		},
+		{
+			question: "Which of the following is command to display lines from 3 to 6?",
+			answers: {
+			  a: "awk 'NR==2, NR==5 {print NR,$0}' employee.txt ",
+			  b: "awk 'NR==3, NR==6 {print NR,$0}' employee.txt ",
+			  c: "awk 'LINE==3, LINE==6 {print NR,$0}' employee.txt ",
+			  
+			},
+			correctAnswer: "b"
+		  }
+		  ,
+		{
+			question: "Which of the following is command to print the line with more than 10 characters ?",
+			answers: {
+			  a: "awk 'length($0) > 10' geeksforgeeks.txt",
+			  b: "grep 'length($0) > 10' geeksforgeeks.txt",
+			  c: "sed 'length($0) > 10' geeksforgeeks.txt ",
+			  
+			},
+			correctAnswer: "a"
+		  }
+	  ];
+
+	  function buildQuiz() {
+	    // we'll need a place to store the HTML output
+	    const output = [];
+
+	    // for each question...
+	    myQuestions.forEach((currentQuestion, questionNumber) => {
+	      // we'll want to store the list of answer choices
+	      const answers = [];
+
+	      // and for each available answer...
+	      for (letter in currentQuestion.answers) {
+	        // ...add an HTML radio button
+	        answers.push(
+	          `<label>
+	             <input type="radio" name="question${questionNumber}" value="${letter}">
+	              ${letter} :
+	              ${currentQuestion.answers[letter]}
+	           </label>`
+	        );
+	      }
+
+	      // add this question and its answers to the output
+	      output.push(
+	        `<div class="slide">
+	           <div class="question"> ${currentQuestion.question} </div>
+	           <div class="answers"> ${answers.join("")} </div>
+	         </div>`
+	      );
+	    });
+
+	    // finally combine our output list into one string of HTML and put it on the page
+	    quizContainer.innerHTML = output.join("");
+	  }
+
+	  function showResults() {
+	    // gather answer containers from our quiz
+	    const answerContainers = quizContainer.querySelectorAll(".answers");
+
+	    // keep track of user's answers
+	    let numCorrect = 0;
+
+	    // for each question...
+	    myQuestions.forEach((currentQuestion, questionNumber) => {
+	      // find selected answer
+	      const answerContainer = answerContainers[questionNumber];
+	      const selector = `input[name=question${questionNumber}]:checked`;
+	      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+	      // if answer is correct
+	      if (userAnswer === currentQuestion.correctAnswer) {
+	        // add to the number of correct answers
+	        numCorrect++;
+
+	        // color the answers green
+	        answerContainers[questionNumber].style.color = "lightgreen";
+	      } else {
+	        // if answer is wrong or blank
+	        // color the answers red
+	        answerContainers[questionNumber].style.color = "red";
+	      }
+	    });
+
+	    // show number of correct answers out of total
+	    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+	  }
+
+	  function showSlide(n) {
+	    slides[currentSlide].classList.remove("active-slide");
+	    slides[n].classList.add("active-slide");
+	    currentSlide = n;
+	    
+	    if (currentSlide === 0) {
+	      previousButton.style.display = "none";
+	    } else {
+	      previousButton.style.display = "inline-block";
+	    }
+	    
+	    if (currentSlide === slides.length - 1) {
+	      nextButton.style.display = "none";
+	      submitButton.style.display = "inline-block";
+	    } else {
+	      nextButton.style.display = "inline-block";
+	      submitButton.style.display = "none";
+	    }
+	  }
+
+	  function showNextSlide() {
+	    showSlide(currentSlide + 1);
+	  }
+
+	  function showPreviousSlide() {
+	    showSlide(currentSlide - 1);
+	  }
+
+	  const quizContainer = document.getElementById("quiz");
+	  const resultsContainer = document.getElementById("results");
+	  const submitButton = document.getElementById("submit");
+
+	  // display quiz right away
+	  buildQuiz();
+
+	  const previousButton = document.getElementById("previous");
+	  const nextButton = document.getElementById("next");
+	  const slides = document.querySelectorAll(".slide");
+	  let currentSlide = 0;
+
+	  showSlide(0);
+
+	  // on submit, show results
+	  submitButton.addEventListener("click", showResults);
+	  previousButton.addEventListener("click", showPreviousSlide);
+	  nextButton.addEventListener("click", showNextSlide);
+	})();
